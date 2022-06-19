249f96058fc88fab7e2b5c185394aba9c171d32d
jinsoobyeon <jinsoobyeon@gmail.com>
2022-06-19 16:38:24 +0900

    home template Vue.js 인스턴스의 컴포넌트로 생성

--- index.js
+++ index.js
@@ -1,49 +1,138 @@
-import { includeHTML } from "./utils.js";
+// import { includeHTML } from "./utils.js";
 
-window.onload = () => {
-  const navLink = document.querySelectorAll(".nav-link");
+// window.onload = () => {
+//   const navLink = document.querySelectorAll(".nav-link");
 
-  const route = (state, url) => {
-    navLink.forEach((link) => {
-      link.classList.remove("active");
-    });
-    document.querySelector(`#${state.title}`).classList.add("active");
-    history.pushState(state, state.title, url);
-    includeHTML(state.file);
-  };
+//   const route = (state, url) => {
+//     navLink.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     document.querySelector(`#${state.title}`).classList.add("active");
+//     history.pushState(state, state.title, url);
+//     includeHTML(state.file);
+//   };
 
-  history.replaceState(
-    { file: "home-page-html", title: "home" },
-    "home",
-    "/#/"
-  );
-  includeHTML("home-page-html");
+//   history.replaceState(
+//     { file: "home-page-html", title: "home" },
+//     "home",
+//     "/#/"
+//   );
+//   includeHTML("home-page-html");
 
-  window.addEventListener("popstate", () => {
-    navLink.forEach((link) => {
-      link.classList.remove("active");
-    });
-    document.querySelector(`#${history.state.title}`).classList.add("active");
-    includeHTML(history.state.file);
-  });
+//   window.addEventListener("popstate", () => {
+//     navLink.forEach((link) => {
+//       link.classList.remove("active");
+//     });
+//     document.querySelector(`#${history.state.title}`).classList.add("active");
+//     includeHTML(history.state.file);
+//   });
 
-  conduit.addEventListener("click", (event) => {
-    event.preventDefault();
-    route({ file: "home-page-html", title: "home" }, "/#/");
-  });
+//   conduit.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     route({ file: "home-page-html", title: "home" }, "/#/");
+//   });
 
-  home.addEventListener("click", (event) => {
-    event.preventDefault();
-    route({ file: "home-page-html", title: "home" }, "/#/");
-  });
+//   home.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     route({ file: "home-page-html", title: "home" }, "/#/");
+//   });
 
-  signIn.addEventListener("click", (event) => {
-    event.preventDefault();
-    route({ file: "login-page-html", title: "signIn" }, "/#/login");
-  });
+//   signIn.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     route({ file: "login-page-html", title: "signIn" }, "/#/login");
+//   });
 
-  signUp.addEventListener("click", (event) => {
-    event.preventDefault();
-    route({ file: "register-page-html", title: "signUp" }, "/#/register");
-  });
+//   signUp.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     route({ file: "register-page-html", title: "signUp" }, "/#/register");
+//   });
+// };
+
+const homePage = {
+  template: `<div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+            <ul class="nav nav-pills outline-active">
+              <li class="nav-item">
+                <a class="nav-link disabled" href="">Your Feed</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="">Global Feed</a>
+              </li>
+            </ul>
+          </div>
+          <div class="article-preview">
+            <div class="article-meta">
+              <a href="profile.html"
+                ><img src="http://i.imgur.com/Qr71crq.jpg"
+              /></a>
+              <div class="info">
+                <a href="" class="author">Eric Simons</a>
+                <span class="date">January 20th</span>
+              </div>
+              <button class="btn btn-outline-primary btn-sm pull-xs-right">
+                <i class="ion-heart"></i> 29
+              </button>
+            </div>
+            <a href="" class="preview-link">
+              <h1>How to build webapps that scale</h1>
+              <p>This is the description for the post.</p>
+              <span>Read more...</span>
+            </a>
+          </div>
+          <div class="article-preview">
+            <div class="article-meta">
+              <a href="profile.html"
+                ><img src="http://i.imgur.com/N4VcUeJ.jpg"
+              /></a>
+              <div class="info">
+                <a href="" class="author">Albert Pai</a>
+               <span class="date">January 20th</span>
+              </div>
+              <button class="btn btn-outline-primary btn-sm pull-xs-right">
+                <i class="ion-heart"></i> 32
+              </button>
+            </div>
+            <a href="" class="preview-link">
+              <h1>
+                The song you won't ever stop singing. No matter how hard you try.
+              </h1>
+              <p>This is the description for the post.</p>
+              <span>Read more...</span>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
+            <div class="tag-list">
+              <a href="" class="tag-pill tag-default">programming</a>
+              <a href="" class="tag-pill tag-default">javascript</a>
+              <a href="" class="tag-pill tag-default">emberjs</a>
+              <a href="" class="tag-pill tag-default">angularjs</a>
+              <a href="" class="tag-pill tag-default">react</a>
+              <a href="" class="tag-pill tag-default">mean</a>
+              <a href="" class="tag-pill tag-default">node</a>
+              <a href="" class="tag-pill tag-default">rails</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`,
 };
+
+new Vue({
+  el: "#wrapper",
+  components: {
+    "home-page": homePage,
+  },
+});
