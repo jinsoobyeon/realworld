05a54c896dcb5e4cd547eb8e29154949734662c0
jinsoobyeon <jinsoobyeon@gmail.com>
2022-06-22 19:18:00 +0900

    로그인 전 네비게이션

--- index.js
+++ index.js
@@ -112,6 +112,38 @@ const LoginPage = {
   </div>`,
 };
 
+const RegisterPage = {
+  template: `<div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <a href="">Have an account?</a>
+          </p>
+          <ul class="error-messages">
+            <li>That email is already taken</li>
+          </ul>
+          <form>
+            <fieldset class="form-group">
+              <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+            </fieldset>
+            <fieldset class="form-group">
+              <input class="form-control form-control-lg" type="text" placeholder="Email">
+            </fieldset>
+            <fieldset class="form-group">
+              <input class="form-control form-control-lg" type="password" placeholder="Password">
+            </fieldset>
+              <button class="btn btn-lg btn-primary pull-xs-right">
+                Sign up
+              </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>`,
+};
+
 const routes = [
   {
     path: "/",
@@ -120,6 +152,10 @@ const routes = [
   {
     path: "/login",
     component: LoginPage,
+  },
+  {
+    path: "/register",
+    component: RegisterPage,
   },
 ];
 
