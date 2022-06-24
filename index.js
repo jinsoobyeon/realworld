9b45bcc87df6e636a658510b6437da6f38eda0a0
jinsoobyeon <jinsoobyeon@gmail.com>
2022-06-24 19:07:24 +0900

    login과 register 페이지 간 이동 링크

--- index.js
+++ index.js
@@ -85,17 +85,14 @@ const LoginPage = {
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign up</h1>
+          <h1 class="text-xs-center">Sign in</h1>
           <p class="text-xs-center">
-            <a href="">Have an account?</a>
+            <router-link to="/register">Need an account?</router-link>
           </p>
           <ul class="error-messages">
             <li>That email is already taken</li>
           </ul>
           <form>
-            <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Your Name">
-            </fieldset>
             <fieldset class="form-group">
               <input class="form-control form-control-lg" type="text" placeholder="Email">
             </fieldset>
@@ -103,7 +100,7 @@ const LoginPage = {
               <input class="form-control form-control-lg" type="password" placeholder="Password">
             </fieldset>
               <button class="btn btn-lg btn-primary pull-xs-right">
-                Sign up
+                Sign in
               </button>
           </form>
         </div>
@@ -119,7 +116,7 @@ const RegisterPage = {
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign up</h1>
           <p class="text-xs-center">
-            <a href="">Have an account?</a>
+            <router-link to="/login">Have an account?</router-link>
           </p>
           <ul class="error-messages">
             <li>That email is already taken</li>
